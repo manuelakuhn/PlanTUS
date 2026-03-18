2cb1b2cafae4d9ed83bd63514fb31122e38001eb
Manuela Kuhn <manuela.kuhn@ovgu.de>
2026-03-18 16:34:28 +0100

    Make other parameter option when getting version in cli

--- src/PlanTUS/PlanTUS_cli.py
+++ src/PlanTUS/PlanTUS_cli.py
@@ -57,14 +57,15 @@ placement_scene_template_filepath = None
 
 def main():
     parser = argparse.ArgumentParser(description="PlanTUS command line interface")
-    parser.add_argument("t1", type=str, help="Path to T1 image")
-    parser.add_argument("mesh", type=str,  help="Path to head mesh")
-    parser.add_argument("roi", type=str, help="Path to target ROI")
+    parser.add_argument("--version", action="store_true", help="Print the version number and exit.")
+    skip_argument="?" if '--version' in sys.argv else 1
+    parser.add_argument("t1", type=str, help="Path to T1 image", nargs=skip_argument)
+    parser.add_argument("mesh", type=str,  help="Path to head mesh", nargs=skip_argument)
+    parser.add_argument("roi", type=str, help="Path to target ROI", nargs=skip_argument)
     parser.add_argument("--config", type=str, help="Path to configuration file")
     parser.add_argument("--skip_wb_view", action="store_true", help="Run calculations but skip wb_view")
     parser.add_argument("--use_internal_viewer", action="store_true", help="Use own viewer instead of wb_view")
     parser.add_argument("--do_only_trajectory", type=int, default=-1, help="Optional integer to run only the generation of trajectory (default: -1). Specify number of triangles to generate.")
-    parser.add_argument("--version", action="store_true", help="Print the version number and exit.")
 
     args = parser.parse_args()
 
